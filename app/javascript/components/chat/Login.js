dc186674918531a6c0ff33ecd535b75641d7f5ef
Pål Andre Sundt <pal@rubynor.com>
2020-10-23 13:39:57 +0200

    Add basic front-end validation

--- app/javascript/components/chat/Login.js
+++ app/javascript/components/chat/Login.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import axios from 'axios';
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import axios from 'axios'
 
 const useStyles = makeStyles({
   root: {
@@ -22,15 +22,42 @@ const useStyles = makeStyles({
 });
 
 export default function Login(props) {
-  const classes = useStyles();
-  const [ email, setEmail ] = useState(props.email);
-  const [ nickname, setNickname ] = useState(props.nickname);
+  const classes = useStyles()
+  const [ email, setEmail ] = useState(props.email)
+  const [ nickname, setNickname ] = useState(props.nickname)
+  const [ submitting, setSubmitting ] = useState(false)
+  const [ validate, setValidate ] = useState(false)
 
-  function login() {
-    axios.post('/login', { user: { email: email, nickname: nickname } })
-      .then(function (response) {
-        window.location = response.data.redirect_path;
-      })
+  useEffect(() => {
+    if (submitting && !nicknameIsInvalid() && !emailIsInvalid()) {
+      axios.post('/login', { user: { email: email, nickname: nickname } })
+        .then(function (response) {
+          window.location = response.data.redirect_path;
+        })
+    }
+
+    return () => {
+      setSubmitting(false)
+    }
+  }, [submitting])
+
+  function login(e) {
+    setValidate(true)
+    setSubmitting(true)
+  }
+
+  function nicknameIsInvalid() {
+    if (!validate)
+      return false
+
+    return nickname.length < 2
+  }
+
+  function emailIsInvalid() {
+    if (!validate)
+      return false
+
+    return !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)
   }
 
   return (
@@ -48,6 +75,7 @@ export default function Login(props) {
             variant="outlined"
             onChange={e => setEmail(e.target.value)}
             value={email}
+            error={emailIsInvalid()}
           />
 
           <TextField
@@ -57,6 +85,7 @@ export default function Login(props) {
             variant="outlined"
             onChange={e => setNickname(e.target.value)}
             value={nickname}
+            error={nicknameIsInvalid()}
           />
 
           <Button
